ebd5510d7a2b1c4b20fd7f61e539e8abd01fdb8a
Jaber-Ali <Jaber.Ali@se.experis.com>
2021-10-18 10:21:20 +0200

    test-Controller

--- src/main/java/se/experis/tidsbankenbackend/controllers/VacationRequestController.java
+++ src/main/java/se/experis/tidsbankenbackend/controllers/VacationRequestController.java
@@ -1,8 +1,45 @@
 package se.experis.tidsbankenbackend.controllers;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import se.experis.tidsbankenbackend.models.User;
+import se.experis.tidsbankenbackend.models.VacationRequest;
+import se.experis.tidsbankenbackend.services.VacationRequestService;
+import java.util.List;
 
 @RestController
 public class VacationRequestController {
+
+    @Autowired
+    private VacationRequestService vacationRequestService;
+
+    /**
+     * Get all VacationRequest
+     *
+     * @return - all VacationRequest
+     */
+    @GetMapping
+    public ResponseEntity<List<VacationRequest>> getAllVacationRequests(User user) {
+        return vacationRequestService.getAllVacationRequests(user);
+    }
+
+    /**
+     * Get specific VacationRequest
+     *
+     * @param id -VacationRequest id
+     * @return - info about that specific VacationRequest
+     */
+    @GetMapping("/{id}")
+    public ResponseEntity<VacationRequest> getVacationRequestById(@PathVariable Long id, User user) {
+        return vacationRequestService.getVacationRequestById(id, user);
+    }
+
+    @PostMapping
+    public ResponseEntity<VacationRequest> addVacationRequest (@RequestBody VacationRequest vacationRequest) {
+
+        return vacationRequestService.addVacationRequest(vacationRequest);
+    }
+
+
 }
--- src/main/java/se/experis/tidsbankenbackend/controllers/VacationRequestStatusController.java
+++ src/main/java/se/experis/tidsbankenbackend/controllers/VacationRequestStatusController.java
@@ -1,7 +1,28 @@
 package se.experis.tidsbankenbackend.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import se.experis.tidsbankenbackend.models.VacationRequestStatus;
+import se.experis.tidsbankenbackend.services.VacationRequestStatusService;
+
+import java.util.List;
 
 @RestController
 public class VacationRequestStatusController {
+
+
+    @Autowired
+    private VacationRequestStatusService vacationRequestStatusService;
+
+    /**
+     * Get all VacationRequestStatus
+     *
+     * @return - all VacationRequestStatus
+     */
+    @GetMapping
+    public ResponseEntity<List<VacationRequestStatus>> getAllVRS() {
+        return vacationRequestStatusService.getAllVRS();
+    }
 }
